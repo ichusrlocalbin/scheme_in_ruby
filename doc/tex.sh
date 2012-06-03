ffa10dc0d3100070f17376b340b57ceb2e2a2330
ichusrlocalbin <ichusrlocalbin@gmail.com>
2012-06-03 11:51:31 +0900

    modify for comments

--- doc/tex.sh
+++ doc/tex.sh
@@ -10,6 +10,15 @@ dvipdfmx ${basename}.dvi && \
 cp ${basename}.pdf ../ && \
 cd .. && \
 cp ${basename}.pdf ../introduction_programming_language.pdf && \
-open ${basename}.pdf
-
-
+basename=kindle
+cd tmp
+cp -r ../images ../*.tex ../*.bib .
+platex ${basename}.tex && \
+jbibtex ${basename} && \
+platex ${basename}.tex && \
+platex ${basename}.tex && \
+dvipdfmx ${basename}.dvi && \
+cp ${basename}.pdf ../ && \
+cd .. && \
+cp ${basename}.pdf ../introduction_programming_language_a5.pdf
+open ../introduction_programming_language.pdf
